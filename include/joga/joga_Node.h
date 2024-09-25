5ad9251f5a4c83859aa41e512232ef82bf0fe2c0
David Spry <dspry@gmx.com>
2024-09-25 16:52:07 +1000

    Add `Node::withSizeAfterLayout`

--- include/joga/joga_Node.h
+++ include/joga/joga_Node.h
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include <utility>
+
 #include <juce_gui_basics/juce_gui_basics.h>
 #include <yoga/Yoga.h>
 
@@ -17,23 +19,34 @@ namespace joga
 class Node final: public impl::LayoutNode,
                   public impl::LayoutNodeInterface<Node> {
   juce::Component& m_component;
+  int m_w = 0;
+  int m_h = 0;
 
 public:
   explicit Node(juce::Component& component): m_component(component) {}
+
+  /// @brief Specify a size to be applied to the component after layout.
+  /// @param size The post-layout size
+  [[nodiscard]]
+  auto withSizeAfterLayout(const juce::Rectangle<int>& size) && -> Node&& {
+    m_w = size.getWidth();
+    m_h = size.getHeight();
+    return std::move(*this);
+  }
 
   /// @brief Notify the node that its layout has been updated.
   /// @param parentOffsetX The x-offset of the node's parent within the layout.
   /// @param parentOffsetY The y-offset of the node's parent within the layout.
   void didUpdateLayout(const float parentOffsetX, const float parentOffsetY) override {
-    m_component.setBounds(
-        juce::Rectangle{
-            YGNodeLayoutGetLeft(this),
-            YGNodeLayoutGetTop(this),
-            YGNodeLayoutGetWidth(this),
-            YGNodeLayoutGetHeight(this)
-        }.translated(parentOffsetX, parentOffsetY)
-         .toNearestInt()
-    );
+    const auto bounds = juce::Rectangle{
+      YGNodeLayoutGetLeft(this),
+      YGNodeLayoutGetTop(this),
+      YGNodeLayoutGetWidth(this),
+      YGNodeLayoutGetHeight(this)
+    }.translated(parentOffsetX, parentOffsetY)
+     .toNearestInt();
+
+    m_component.setBounds(m_w > 0 && m_h > 0 ? bounds.withSizeKeepingCentre(m_w, m_h) : bounds);
   }
 };
 
